de381816ac7d7762566bb73f56436ae65f722dd0
victorgsp <victorgsp@gmail.com>
2019-02-01 12:28:32 -0300

    start

---
+++ generate-csv-to-google.js
@@ -0,0 +1,57 @@
+//put your contact marker in next variable (marker)
+var contactMarker = "PUT YOUR MARKER HERE";
+
+var contacts = "Name,Given Name,Additional Name,Family Name,Yomi Name,Given Name Yomi,Additional Name Yomi,Family Name Yomi,Name Prefix,Name Suffix,Initials,Nickname,Short Name,Maiden Name,Birthday,Gender,Location,Billing Information,Directory Server,Mileage,Occupation,Hobby,Sensitivity,Priority,Subject,Notes,Language,Photo,Group Membership,Phone 1 - Type,Phone 1 - Value\n";
+
+(function(){
+  var newscript = document.createElement('script');
+     newscript.type = 'text/javascript';
+     newscript.async = true;
+     newscript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js';
+  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
+})();
+
+
+
+function downloadFile(filename, data) {
+    var blob = new Blob([data], {type: 'text/csv'});
+    if(window.navigator.msSaveOrOpenBlob) {
+        window.navigator.msSaveBlob(blob, filename);
+    }
+    else{
+        var elem = window.document.createElement('a');
+        elem.href = window.URL.createObjectURL(blob);
+        elem.download = filename;        
+        document.body.appendChild(elem);
+        elem.click();        
+        document.body.removeChild(elem);
+    }
+}
+
+function generateContactFileToImport(){
+    $('._3j7s9').each(function(index, item){
+      
+        try{
+            let contactName = $(item).find('._1qP8m > span')[0].innerText;
+            let contactPhone = $(item).find("._1wjpf")[0].innerText.replace('+55 81 ', 9);
+            if(contactMarker != null && contactMarker != ""){
+              contactMarker = " " + contactMarker;
+            }else{
+              contactMarker = "";
+            }
+
+            contacts += `${contactName}${contactMarker},${contactName}${contactMarker},,,,,,,,,,,,,,,,,,,,,,,,,,,* myContacts,Mobile,${contactPhone}\n`;  
+            
+        }catch(err){
+            console.log(err);
+        }
+
+    });    
+}
+
+function downloadContactFileToImport(){
+    generateContactFileToImport();
+    downloadFile('contacts.csv', contacts);
+}
+
+setTimeout(downloadContactFileToImport, 1000);;
